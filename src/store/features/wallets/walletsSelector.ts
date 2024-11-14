c5b764f6a20f56f666e22ff383a0ea1a320ccaca
BenoistP <75934369+BenoistP@users.noreply.github.com>
2024-11-14 18:34:51 +0100

    now useless eslint-disable

--- src/store/features/wallets/walletsSelector.ts
+++ src/store/features/wallets/walletsSelector.ts
@@ -52,7 +52,6 @@ export interface OtherRealtoken {
   unitPriceCost: number
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface RWARealtoken extends OtherRealtoken {
   initialLaunchDate: APIRealTokenDate
 }
